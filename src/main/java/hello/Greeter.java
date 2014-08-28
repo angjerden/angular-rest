4c0abf03cbc890ab48940fa27c30ea47833eb890
Anders Gjerde <angjerden@gmail.com>
2014-08-28 11:02:18 +0200

    HelloWorld class, gradle build works

---
+++ src/main/java/hello/Greeter.java
@@ -0,0 +1,10 @@
+package hello;
+
+/**
+ * Created by anders on 27.08.14.
+ */
+public class Greeter {
+    public String sayHello() {
+        return "Hello World!";
+    }
+}
---
+++ src/main/java/hello/HelloWorld.java
@@ -0,0 +1,11 @@
+package hello;
+
+/**
+ * Created by anders on 27.08.14.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        Greeter greeter = new Greeter();
+        System.out.println(greeter.sayHello());
+    }
+}
