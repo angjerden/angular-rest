98c97a730bb2a5ab63a9ee4003de7ad505196090
Anders Gjerde <angjerden@gmail.com>
2014-08-29 10:08:00 +0200

    Added SpringApplication, commented out HelloWorld main method

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -6,11 +6,11 @@ import org.joda.time.LocalTime;
  * Created by anders on 27.08.14.
  */
 public class HelloWorld {
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         LocalTime currentTime = new LocalTime();
         System.out.println("The current local time is: " + currentTime);
 
         Greeter greeter = new Greeter();
         System.out.println(greeter.sayHello());
-    }
+    }*/
 }
