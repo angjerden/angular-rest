a82126ef6e39714e239b45642665809c20ecfdd5
Anders Gjerde <angjerden@gmail.com>
2014-08-29 10:15:29 +0200

    Forgot closing quotation mark on title of guide

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -8,7 +8,7 @@ import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * Created by anders on 28.08.14.
- * Based on the Spring Guide: "Building a RESTful Web Service,
+ * Based on the Spring Guide: "Building a RESTful Web Service",
  * found at: https://spring.io/guides/gs/rest-service/
  */
 @RestController
